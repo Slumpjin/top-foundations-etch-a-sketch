c54a46e77f17a0f3058cc8214e945cb019e5dc3a
Slumpjin <tap4302@gmail.com>
2024-02-27 22:45:38 -0600

    Add button to prompt user for grid side length

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
+const MAX_SIDE_LENGTH = 100;
+const DEFAULT_SIDE_LENGTH = 16;
+let userDefinedSideLength = 0;
+
 const canvasDiv = document.querySelector('.canvas');
-
-for (let i = 0; i < 16; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.setAttribute('style', 'display: flex; flex-grow: 1;')
-    for (let i = 0; i < 16; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.setAttribute('style', 'flex-grow: 1; border: 1px solid black');
-        
-        gridRow.appendChild(gridItem);
-    }
-    canvasDiv.appendChild(gridRow);
-}
-
 canvasDiv.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = 'black';
 });
+createGrid(DEFAULT_SIDE_LENGTH);
+
+const button = document.querySelector('button');
+button.addEventListener('click', (e) => {
+    userDefinedSideLength = Number(prompt(`Enter the number of blocks you'd like a side of the grid to have`));
+    if (userDefinedSideLength > 0 && userDefinedSideLength <= MAX_SIDE_LENGTH) {
+        while (canvasDiv.firstChild) canvasDiv.removeChild(canvasDiv.firstChild);
+        createGrid(userDefinedSideLength);
+    }
+})
+
+function createGrid(sideLength) {
+    for (let i = 0; i < sideLength; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.setAttribute('style', 'display: flex; flex-grow: 1;')
+        for (let i = 0; i < sideLength; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.setAttribute('style', 'flex-grow: 1; border: 1px solid black');
+            
+            gridRow.appendChild(gridItem);
+        }
+        canvasDiv.appendChild(gridRow);
+    }
+};
